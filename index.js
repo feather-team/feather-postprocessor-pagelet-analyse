18dd5030553405e3c1ff93ac3dd333817a371205
jsyczhanghao <jsyczhanghao@sina.com>
2014-12-17 12:30:13 +0800

    pagelet update

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 module.exports = function(content, file, conf){
     if(file.isPageletLike && file.isHtmlLike){
-    	var id = file.subpathNoExt.replace(/\//g, '_'), pageletId;
-    	var sameCss = feather.file.wrap(file.id.replace(/\.[^\.]+$/, '.css'));
-	    var sameJs = feather.file.wrap(file.id.replace(/\.[^\.]+$/, '.js'));
+        var id = file.subpathNoExt.replace(/\//g, '_');
+        var sameCss = feather.file.wrap(file.id.replace(/\.[^\.]+$/, '.css'));
+        var sameJs = feather.file.wrap(file.id.replace(/\.[^\.]+$/, '.js'));
 
-    	var tmp = content.match(/<!--pagelet:((?:(?!-->)[\s\S])+)-->/i);
+        var tmp = content.match(/<!--pagelet:((?:(?!-->)[\s\S])+)-->/i);
 
-    	if(tmp){
-    		pageletId = tmp[1];
+        if(tmp){
+            tmp = tmp[1].split(':');
 
-	    	content = '<div id="' + id + '" style="display: none !important;">' + content + '</div>';
+            var pageletId = tmp[0], type = (tmp[1] || 'textarea').toLowerCase(), reg = new RegExp('<\\/' + type + '>', 'g');
 
-	        var async1 = ['/static/js/pagelet.js'];
+            if(['script', 'style', 'textarea'].indexOf(type.toLowerCase()) > -1){
+                content = content.replace(new RegExp('<\\/' + type + '>', 'g'), '<\\\/' + type + '>');
+            }
 
-	        if(sameCss.exists()){
-	        	async1.push(sameCss.subpath);
-	        }
+            content = '<' + type + ' id="' + id + '" style="display: none !important;" type="text/html">' + content + '</' + type + '>';
 
-	        content += '<script>require.async(' + feather.util.json(async1) + ',function(PageLet){\
-PageLet("' + id + '","' + pageletId + '");\
+            var async1 = ['/static/js/pagelet.js'];
+
+            if(sameCss.exists()){
+                async1.push(sameCss.subpath);
+            }
+
+            content += '<script>require.async(' + feather.util.json(async1) + ',function(PageLet){\
+PageLet("' + id + '","' + pageletId + '","' + type + '");\
 ' + (sameJs.exists() ? 'require.async("' + sameJs.subpath + '")' : '') + '});</script>';
 
             file.addRequire('/static/js/pagelet.js');
             file.addRequire(sameCss.id);
-    	}else{
+        }else{
             if(sameCss.exists()){
                 content += '<link rel="stylesheet href="' + sameCss.subpath + '" type="text/css" />';
             }
@@ -31,7 +37,7 @@ PageLet("' + id + '","' + pageletId + '");\
             if(sameJs.exists()){
                 content += '<script>require.async("' + sameJs.subpath + '");</script>';
             }
-    	}
+        }
 
         file.addRequire(sameJs.id);
     }
